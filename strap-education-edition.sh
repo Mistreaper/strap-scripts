7302f37fbfd1b8aa5c6c39a40c19fd9920437c8e
Mistreaper <realmistreaper@gmail.com>
2022-04-04 20:42:28 +0700

    added Chaotic AUR for education edition

--- strap-education-edition.sh
+++ strap-education-edition.sh
@@ -37,7 +37,16 @@ if ! [ -x "$(command -v yay)" ]; then
     cd ..
     sudo rm -r yay-bin
     echo "When installing something with yay, press enter to continue"
-fi 
+fi
+
+# install Chaotic AUR (precompiled AUR binaries)
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036 
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' 
+echo '[chaotic-aur]' | sudo tee -a /etc/pacman.conf> /dev/null 
+echo 'Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf > /dev/null
+sudo pacman -Sy # sync chaotic AUR
+
 echo "Initializing guest user"
 yay -S --needed systemd-guest-user 
 sleep 2s
